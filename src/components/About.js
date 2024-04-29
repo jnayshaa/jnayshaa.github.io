e42b9c8e3257a1c8b8350457ca1a2e3d5ee1cbe3
Naysha Jain <jnaysha@gmail.com>
2024-04-28 23:17:53 -0500

    Update About component to include profile image

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from "react";
+import myImage from '../images/profile.PNG'; // Import your image file
 
 function About() {
     return (
         <div>
         <h1 style={{ color: '#fff' }}>About Me</h1>
-        <p>My name is John Doe and I am a web developer.</p>
+        <p style={{ color: '#fff' }}>I am a web developer.</p>
+        <img src={myImage} alt="My Photo" />
+
         </div>
     );
     }
