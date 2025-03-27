827b23ac3cd42e8c7c317342a0626fe0e72dc7c3
Naysha <njain@knox.edu>
2025-03-27 14:28:29 -0500

    Add react-responsive and update resume URL

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import background from "../images/home_bg.png";
 import Mbackground from "../images/profile.PNG";
+import useMediaQuery from 'react-responsive';
 
 function Home() {
     const rotatingTexts = ["a student", "a developer", "an astronomer", "a creator"];
     const [currentText, setCurrentText] = useState(rotatingTexts[0]);
     const [displayedText, setDisplayedText] = useState('');
     const [index, setIndex] = useState(0);
+
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     useEffect(() => {
         const changeText = () => {
@@ -37,15 +40,15 @@ function Home() {
     }, [currentText]);
 
     return (
-        <div 
-            className="flex justify-end items-center min-h-screen w-full bg-cover bg-center bg-no-repeat -mt-30"
-            style={{ backgroundImage: `url(${background})` }}
+        <div
+            className={`flex justify-end items-center min-h-screen w-full bg-cover bg-center bg-no-repeat`}
+            style={{ backgroundImage: `url(${isMobile ? Mbackground : background})` }}
         >
-            <div className="flex flex-col justify-center items-start w-1/2 text-left px-5 text-white">
-                <p className="font-sans m-0 p-1 text-3xl">Hello! I am </p>
-                <p className="font-montserrat m-0 p-1 text-9xl">Naysha</p>
-                <p className="font-sans m-0 p-1 text-9xl">Jain</p>
-                <p className="font-sans m-0 p-1 text-3xl">I am {displayedText}</p>
+            <div className={`flex flex-col justify-center items-start ${isMobile ? 'text-center w-full px-3' : 'w-1/2 text-left px-5'} text-white`}>
+                <p className={`font-sans m-0 p-1 ${isMobile ? 'text-xl' : 'text-3xl'}`}>Hello! I am </p>
+                <p className={`font-montserrat m-0 p-1 ${isMobile ? 'text-5xl' : 'text-9xl'}`}>Naysha</p>
+                <p className={`font-sans m-0 p-1 ${isMobile ? 'text-5xl' : 'text-9xl'}`}>Jain</p>
+                <p className={`font-sans m-0 p-1 ${isMobile ? 'text-xl' : 'text-3xl'}`}>I am {displayedText}</p>
             </div>
         </div>
     );
